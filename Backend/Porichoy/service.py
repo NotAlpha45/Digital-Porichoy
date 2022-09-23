9b42389b0f86ebec7a5160361d640665d043a79e
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-09-24 03:53:29 +0600

    Modularized instance creation

--- Backend/Porichoy/service.py
+++ Backend/Porichoy/service.py
@@ -10,7 +10,8 @@ from .ServiceHandler.service_handler import create_service
 
 async def create_service(request: HttpRequest):
 
-    request_body = json.loads(request.body.decode("utf-8"))
-    print(request_body)
-    # return HttpResponse("OK")
-    return await create_service(request_body)
+    request_body = json.loads(request.decode("utf-8"))
+    print(type(request_body))
+    # print(type(json.loads(request.body.decode("utf-8"))))
+    return HttpResponse("OK")
+    # return await create_service(request_body)
