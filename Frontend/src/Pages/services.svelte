910a5c73ab0ef6e62aa15d4e337d2be3ceab3061
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-12-26 23:18:34 +0600

    Changed form of product search

--- Frontend/src/Pages/services.svelte
+++ Frontend/src/Pages/services.svelte
@@ -1,24 +1,25 @@
 <script>
   import axios from "axios";
-  import router from "page";
+  import router, { show } from "page";
   import { serviceIdStore } from "../utility_functions";
   import Geolocation from "svelte-geolocation";
 
-  let category = null,
-    shopIndex = null,
-    coords = [-1, -1];
+  let coords = [-1, -1];
 
   function getStore() {
     router.redirect("/store");
   }
 
-  let shops = [];
+  let shops = [],
+    category = "Mechanic/মেকানিক",
+    district = "Gazipur/গাজীপুর",
+    showAll = false;
 
-  async function mockGetStore() {
+  async function mockGetStore(category, district) {
     await axios
       .get("http://127.0.0.1:8000/services/search_service", {
         params: {
-          district: "gazipur",
+          district: district,
           category: category,
           long: coords[0],
           lat: coords[1],
@@ -32,43 +33,118 @@
       });
   }
 
+  function submit() {
+    // get the selected values of the dropdowns and checkbox
+    category = document
+      .getElementById("category")
+      .value.split("/")[0]
+      .toLowerCase();
+    district = document
+      .getElementById("district")
+      .value.split("/")[0]
+      .toLowerCase();
+    showAll = document.getElementById("show-all").checked;
+
+    // run the function using the selected values
+    // runFunction(category, district, showAll);
+    // console.log(category);
+    // console.log(district);
+  }
+
   // This section activates whenever an element (category is changed)
   $: {
     navigator.geolocation.getCurrentPosition(function (location) {
       coords[0] = location.coords.longitude;
       coords[1] = location.coords.latitude;
     });
-    if (category !== null && coords !== []) { 
-      mockGetStore();
+
+    if (coords !== [] && category !== null && district !== null) {
+      let selectedCategory = category.split("/")[0].toLowerCase();
+      let selectedDistrict = district.split("/")[0].toLowerCase();
+      mockGetStore(selectedCategory, selectedDistrict);
     }
+    console.log(category, district, coords, showAll, $serviceIdStore);
   }
 </script>
 
 <section id="portfolio" class="portfolio sections-bg">
   <div class="container" data-aos="fade-up">
     <div class="section-header">
-      <h2>Stores and Services</h2>
-    </div>
+      <h2>Search For Service</h2>
+      <div class="row gx-lg-0 gy-4 justify-content-center">
+        <div class="col-4">
+          <div class="form-group mt-3 align-items-center">
+            <h5>খোঁজার অপশন দিন</h5>
+          </div>
 
-    <div class="row gx-lg-0 gy-4 justify-content-center">
-      <div class="col-4">
-        <div class="form-group mt-3 align-items-center">
-          <h5>ধরণ পছন্দ করুন</h5>
-        </div>
+          <div class="form-container">
+            <form class="form-inline">
+              <div class="form-group mb-2">
+                <label for="category" class="sr-only">ধরণ পছন্দ করুন</label>
+                <select
+                  class="form-control"
+                  id="category"
+                  bind:value={category}
+                >
+                  <option>Mechanic/মেকানিক</option>
+                  <option>Service/সার্ভিস</option>
+                  <option>Business/ব্যবসা</option>
+                </select>
+              </div>
+              <div class="form-group mb-2">
+                <label for="district" class="sr-only">জেলা পছন্দ করুন</label>
+                <select
+                  class="form-control"
+                  id="district"
+                  bind:value={district}
+                >
+                  <option>Gazipur/গাজীপুর</option>
+                  <option>Dhaka/ঢাকা</option>
+                  <option>Rajshahi/রাজশাহী</option>
+                  <option>Rangpur/রংপুর</option>
+                </select>
+              </div>
+              <div class="form-check mb-2">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="show-all"
+                  bind:checked={showAll}
+                />
+                <label class="form-check-label" for="show-all"
+                  >সব দোকান/সেবা দেখান</label
+                >
+              </div>
+              <!-- <button
+                type="submit"
+                class="btn btn-success rounded-pill"
+                on:click={submit}>দোকান/সার্ভিস খুঁজুন</button
+              > -->
+            </form>
+          </div>
 
-        <div class="form-group mt-3">
-          <select
-            class="form-select"
-            aria-label="select-store"
-            bind:value={category}
-            placeholder="Select"
-          >
-            <option value="mechanic">Mechanic-মেকানিক</option>
-            <option value="business">Business-ব্যবসা</option>
-            <option value="labour">Labour-শ্রমিক</option>
-          </select>
+          <!-- <div class="form-group mt-3">
+            <select
+              class="form-select"
+              aria-label="select-store"
+              bind:value={category}
+              placeholder="Select"
+            >
+              <option value="mechanic">Mechanic-মেকানিক</option>
+              <option value="business">Business-ব্যবসা</option>
+              <option value="labour">Labour-শ্রমিক</option>
+            </select>
+          </div> -->
         </div>
       </div>
+    </div>
+  </div>
+</section>
+
+<section id="portfolio" class="portfolio sections-bg">
+  <div class="container" data-aos="fade-up">
+    <div class="section-header">
+      <h2>Stores and Services</h2>
     </div>
 
     <div
@@ -79,7 +155,6 @@
       data-aos="fade-up"
       data-aos-delay="10"
     >
-
       <div class="row gy-4 portfolio-container">
         {#each shops as shop}
           <div class="col-xl-4 col-md-6 portfolio-item filter-app">
@@ -102,7 +177,7 @@
                     class="btn btn-success rounded-pill"
                     on:click={getStore}
                     on:click={() => {
-                      shopIndex = shop.credentials.provider_id;
+                      let shopIndex = shop.credentials.provider_id;
                       serviceIdStore.set(shopIndex);
                     }}
                   >
@@ -118,115 +193,6 @@
           </div>
           <!-- End Portfolio Item -->
         {/each}
-        <!-- <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/product-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-1.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Product 1</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item -->
-
-        <!-- <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/branding-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-1.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Branding 1</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/books-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-1.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Books 1</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/app-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-2.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">App 2</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/product-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-2.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Product 2</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/branding-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-2.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Branding 2</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/books-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-2.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Books 2</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/app-3.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-3.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">App 3</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/product-3.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-3.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Product 3</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/branding-3.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-3.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Branding 3</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div><!-- End Portfolio Item 
-
-          <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-            <div class="portfolio-wrap">
-              <a href="assets/img/portfolio/books-3.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-3.jpg" class="img-fluid" alt=""></a>
-              <div class="portfolio-info">
-                <h4><a href="portfolio-details.html" title="More Details">Books 3</a></h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-              </div>
-            </div>
-          </div>End Portfolio Item -->
       </div>
       <!-- End Portfolio Container -->
     </div>
@@ -235,4 +201,10 @@
 
 <!-- End Portfolio Section -->
 <style>
+  .form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
 </style>
